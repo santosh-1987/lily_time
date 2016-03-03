5f2a22f961ad93ceafa33b812570af5ccf3eaa8d
Santosh Kumar Mohanty <santoshkumar.mohanty@timeinc.com>
2016-03-03 20:34:34 +0530

    Added Source Code

--- lib/lily_time.rb
+++ lib/lily_time.rb
@@ -1,5 +1,52 @@
 require "lily_time/version"
+require "restclient"
+require "pry"
+require "json"
 
 module LilyTime
-  # Your code goes here...
+  class Scanner
+    URL = "http://localhost:12060/repository/scan"
+    DATA = { :recordFilter => { "@class" => "org.lilyproject.repository.api.filter.RecordTypeFilter",:recordType => "{my.demo}product"},:caching => 10 }
+    attr_accessor :scanner
+    def initialize(args={})
+      DATA["recordType"] = args[:recordType] if args[:recordType].present?
+      puts  DATA
+      rest_response = RestClient.post( URL, DATA.to_json,:content_type => :json)
+      @scanner = rest_response.code == 201 ? rest_response.raw_headers["location"] : nil
+    end
+
+    def scan_records
+      if @scanner.empty?
+        puts "Scanner is Empty, Try Again"
+      else
+        status_code = 200
+        until status_code != 200
+          response = fetch_scanner
+          status_code = response.code
+          if status_code != 200
+            puts "Triggering DELETE"
+            response = RestClient.delete(@scanner.first)
+            puts response.code
+          else
+            result = JSON.parse(response.body)
+            result_set = result["results"]
+            puts  result_set
+          end
+        end
+      end
+    end
+
+    def self.run_program
+      lily_scanner = LilyTime::Scanner.new
+      lily_scanner.scan_records
+    end
+
+    private
+
+    def fetch_scanner
+      RestClient.get((@scanner.first)+("?batch=10"))
+    end
+
+  end
+
 end
--- lib/lily_time/version.rb
+++ lib/lily_time/version.rb
@@ -1,3 +1,3 @@
 module LilyTime
-  VERSION = "0.1.0"
+  VERSION = "1.0.Beta"
 end
